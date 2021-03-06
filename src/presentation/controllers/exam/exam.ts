eeeeb96aa37af765b928085fa4cef33fdd43a39a
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-06 11:35:02 -0300

    feat: ensure ExamController returns 400 if an invalid type is provided

--- src/presentation/controllers/exam/exam.ts
+++ src/presentation/controllers/exam/exam.ts
@@ -1,14 +1,18 @@
 import { AddExam } from '../../../domain/usecases/add-exam'
+import { InvalidParamError } from '../../errors/invalid-param-error'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { badRequest, ok, serverError } from '../../helpers/http-helper'
 import { Controller } from '../../protocols/controller'
+import { ExamTypeValidator } from '../../protocols/exam-type-validator'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class ExamController implements Controller {
   private readonly addExam: AddExam
+  private readonly examTypeValidator: ExamTypeValidator
 
-  constructor (addExam: AddExam) {
+  constructor (addExam: AddExam, examTypeValidator: ExamTypeValidator) {
     this.addExam = addExam
+    this.examTypeValidator = examTypeValidator
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -18,6 +22,11 @@ export class ExamController implements Controller {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
+      }
+
+      const isExamType = this.examTypeValidator.isExamType(httpRequest.body.type)
+      if (!isExamType) {
+        return badRequest(new InvalidParamError('type'))
       }
 
       const { name, description, type, questions } = httpRequest.body
