92cdb7e072bf561a02ee4f34d454b87f5617f99f
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-08 16:37:15 -0300

    refactor: connect mongodb before integration tests

--- src/main/routes/exam-routes.test.ts
+++ src/main/routes/exam-routes.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('Exam Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const examCollection = MongoHelper.getCollection('exams')
+    await examCollection.deleteMany({})
+  })
+
   test('Should return an exam on success', async () => {
     await request(app)
       .post('/api/exam')
