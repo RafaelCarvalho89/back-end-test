226e58ce2ed041faf316ac300cec0b446c4a3362
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-08 14:27:14 -0300

    refactor: create mapper helper method and data delete for tests

--- src/infra/db/mongodb/exam-repository/exam.ts
+++ src/infra/db/mongodb/exam-repository/exam.ts
@@ -7,7 +7,6 @@ export class ExamMongoRepository implements AddExamRepository {
   async add (examData: AddExamModel): Promise<ExamModel> {
     const examCollection = MongoHelper.getCollection('exams')
     const result = await examCollection.insertOne(examData)
-    const { _id, ...examWithoutId } = result.ops[0]
-    return Object.assign({}, examWithoutId, { id: _id })
+    return MongoHelper.map(result.ops[0])
   }
 }
