c13fb7768924900c5b9ac488931454ecad063db4
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-09 10:50:43 -0300

    refactor: rename file

--- src/main/routes/exam-routes.ts
+++ src/main/routes/exam-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express.route-adapter'
-import { makeExamController } from '../factories/exam'
+import { makeExamController } from '../factories/exam-factory'
 
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
