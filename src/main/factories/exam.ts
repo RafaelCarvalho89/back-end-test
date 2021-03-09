abe23a0a199601da514e301c801b49cc79b11413
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-09 08:23:01 -0300

    fix: import path

--- src/main/factories/exam.ts
+++ src/main/factories/exam.ts
@@ -1,6 +1,6 @@
 import { DbAddExam } from '../../data/usecases/add-exam/db-add-exam'
-import { ExamMongoRepository } from '../../infra/db/mongodb/exam-repository/exam'
-import { ExamController } from '../../presentation/controllers/exam/exam'
+import { ExamMongoRepository } from '../../infra/db/mongodb/exam-repository/exam-mongo-repository'
+import { ExamController } from '../../presentation/controllers/exam/exam-controller'
 import { ExamTypeValidatorAdapter } from '../../utils/exam-type-validator/exam-type-validator'
 
 export const makeExamController = (): ExamController => {
