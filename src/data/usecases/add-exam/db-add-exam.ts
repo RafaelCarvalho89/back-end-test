a648cbf39aaf2bc1e54467c9daa28516bb56a1c3
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-09 17:36:27 -0300

    refactor: remove constructor redundancy

--- src/data/usecases/add-exam/db-add-exam.ts
+++ src/data/usecases/add-exam/db-add-exam.ts
@@ -6,11 +6,7 @@ import {
 } from './db-add-exam-protocols'
 
 export class DbAddExam implements AddExam {
-  private readonly addExamRepository: AddExamRepository
-
-  constructor (addExamRepository: AddExamRepository) {
-    this.addExamRepository = addExamRepository
-  }
+  constructor (private readonly addExamRepository: AddExamRepository) { }
 
   async add (examData: AddExamModel): Promise<ExamModel> {
     const exam = await this.addExamRepository.add(examData)
