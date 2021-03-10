65702d3892a19883f176bf4b209e73b4bc3c6d02
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-09 23:54:35 -0300

    feat: ensure ExamMongo repository return an exam id on success when update

--- src/infra/db/mongodb/exam-repository/exam-mongo-repository.ts
+++ src/infra/db/mongodb/exam-repository/exam-mongo-repository.ts
@@ -1,12 +1,22 @@
-import { AddExamRepository } from '../../../../data/protocols/exam-repository/add-exam-repository'
-import { AddExamModel } from '../../../../domain/usecases/exam/add-exam'
+import { ExamRepository } from '../../../../data/protocols/exam-repository/exam-repository'
+import { AddExamModel, UpdateExamModel } from '../../../../domain/usecases/exam'
 import { ExamModel } from '../../../../domain/models/exam/exam-model'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class ExamMongoRepository implements AddExamRepository {
+export class ExamMongoRepository implements ExamRepository {
   async add (examData: AddExamModel): Promise<ExamModel> {
     const examCollection = MongoHelper.getCollection('exams')
     const result = await examCollection.insertOne(examData)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async update (examData: UpdateExamModel): Promise<any> {
+    const examCollection = MongoHelper.getCollection('exams')
+    const { upsertedId } = await examCollection.updateOne(
+      { _id: examData.id },
+      { $set: examData },
+      { upsert: true }
+    )
+    return { id: upsertedId._id }
   }
 }
