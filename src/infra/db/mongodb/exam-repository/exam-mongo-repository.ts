044be8305e6f90ab23b16947850f276d02cbd118
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-11 15:39:31 -0300

    feat: ensure mongodb reconnect is connection if client is down

--- src/infra/db/mongodb/exam-repository/exam-mongo-repository.ts
+++ src/infra/db/mongodb/exam-repository/exam-mongo-repository.ts
@@ -10,13 +10,13 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class ExamMongoRepository implements ExamRepository {
   async add (examData: AddExamModel): Promise<ExamModel> {
-    const examCollection = MongoHelper.getCollection('exams')
+    const examCollection = await MongoHelper.getCollection('exams')
     const result = await examCollection.insertOne(examData)
     return MongoHelper.map(result.ops[0])
   }
 
   async update (examData: UpdateExamModel): Promise<any> {
-    const examCollection = MongoHelper.getCollection('exams')
+    const examCollection = await MongoHelper.getCollection('exams')
     const { upsertedId } = await examCollection.updateOne(
       { _id: examData.id },
       { $set: examData },
@@ -26,7 +26,7 @@ export class ExamMongoRepository implements ExamRepository {
   }
 
   async get (examData: GetExamModel): Promise<ExamModel> {
-    const examCollection = MongoHelper.getCollection('exams')
+    const examCollection = await MongoHelper.getCollection('exams')
     const exam = await examCollection.findOne({
       _id: examData.id
     })
@@ -34,13 +34,13 @@ export class ExamMongoRepository implements ExamRepository {
   }
 
   async list (): Promise<ExamModel[]> {
-    const examCollection = MongoHelper.getCollection('exams')
+    const examCollection = await MongoHelper.getCollection('exams')
     const examList = await examCollection.find().toArray()
     return examList.map((collection) => MongoHelper.map(collection))
   }
 
   async delete (examData: DeleteExamModel): Promise<any> {
-    const examCollection = MongoHelper.getCollection('exams')
+    const examCollection = await MongoHelper.getCollection('exams')
     const { result: { n, ...ok } } = await examCollection.deleteOne({
       _id: examData.id
     })
