be23f8853b3626f9ba91cbeff59496e822204cd0
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-10 21:57:44 -0300

    feat: implement exam mongo repository get method

--- src/infra/db/mongodb/exam-repository/exam-mongo-repository.ts
+++ src/infra/db/mongodb/exam-repository/exam-mongo-repository.ts
@@ -1,5 +1,9 @@
 import { ExamRepository } from '../../../../data/protocols/exam-repository/exam-repository'
-import { AddExamModel, UpdateExamModel } from '../../../../domain/usecases/exam'
+import {
+  AddExamModel,
+  GetExamModel,
+  UpdateExamModel
+} from '../../../../domain/usecases/exam'
 import { ExamModel } from '../../../../domain/models/exam/exam-model'
 import { MongoHelper } from '../helpers/mongo-helper'
 
@@ -18,5 +22,13 @@ export class ExamMongoRepository implements ExamRepository {
       { upsert: true }
     )
     return { id: upsertedId._id }
+  }
+
+  async get (examData: GetExamModel): Promise<ExamModel> {
+    const examCollection = MongoHelper.getCollection('exams')
+    const exam = await examCollection.findOne({
+      _id: examData.id
+    })
+    return MongoHelper.map(exam)
   }
 }
