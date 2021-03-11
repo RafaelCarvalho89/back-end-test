77839e3e699b5142154e748fb484c7275154c765
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-11 10:49:56 -0300

    fix: set Promise type in GetExam interface

--- src/domain/usecases/exam/get-exam.ts
+++ src/domain/usecases/exam/get-exam.ts
@@ -1,7 +1,9 @@
+import { ExamModel } from '../../models/exam/exam-model'
+
 export interface GetExamModel {
   id: string
 }
 
 export interface GetExam {
-  get: (exam: GetExamModel) => Promise<any>
+  get: (exam: GetExamModel) => Promise<ExamModel>
 }
