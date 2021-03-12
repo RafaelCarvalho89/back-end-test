64c4491ebf86690a1735a6f69148994083715532
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-12 08:33:42 -0300

    test: implement DbDeleteQuestion usecase in data layer

---
+++ src/data/usecases/question/delete-question/db-delete-questions.spec.ts
@@ -0,0 +1,85 @@
+import { QuestionModel } from '../../../../domain/models/question/question-model'
+import { DeleteQuestionModel } from '../../../../domain/usecases/question/delete-question'
+import { DeleteQuestionRepository } from '../../../protocols/question-repository/delete-question-repository'
+import { DbDeleteQuestion } from './db-delete-questions'
+
+const makeFakeQuestion = (): QuestionModel => ({
+  id: '607b9974-4914-44df-81e8-d56ec6a58912',
+  statement: 'Qual o sentido da vida, do universo e de tudo mais?',
+  options: [
+    {
+      id: '607b9974-4914-44df-81e8-d56ec6a58951',
+      key: 'a',
+      value: 'viver',
+      correct: false
+    },
+    {
+      id: '607b9974-4914-44df-81e8-d56ec6a58952',
+      key: 'b',
+      value: 'beber café',
+      correct: false
+    },
+    {
+      id: '607b9974-4914-44df-81e8-d56ec6a58953',
+      key: 'c',
+      value: 'codar',
+      correct: false
+    },
+    {
+      id: '607b9974-4914-44df-81e8-d56ec6a58954',
+      key: 'd',
+      value: '42',
+      correct: true
+    }
+  ]
+})
+
+const makeFakeDeleteQuestion = (): DeleteQuestionModel => ({
+  id: '607b9974-4914-44df-81e8-d56ec6a58912'
+})
+
+const makeDeleteQuestionRepository = (): DeleteQuestionRepository => {
+  class DeleteQuestionRepositoryStub implements DeleteQuestionRepository {
+    async delete (QuestionData: DeleteQuestionModel): Promise<any> {
+      return await new Promise((resolve) => resolve(makeFakeQuestion()))
+    }
+  }
+  return new DeleteQuestionRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbDeleteQuestion
+  deleteQuestionRepositoryStub: DeleteQuestionRepository
+}
+
+const makeSut = (): SutTypes => {
+  const deleteQuestionRepositoryStub = makeDeleteQuestionRepository()
+  const sut = new DbDeleteQuestion(deleteQuestionRepositoryStub)
+  return { sut, deleteQuestionRepositoryStub }
+}
+
+describe('DbDeleteQuestion Use case', () => {
+  test('Should call DeleteQuestionRepository with correct values', async () => {
+    const { sut, deleteQuestionRepositoryStub } = makeSut()
+    const deleteSpy = jest.spyOn(deleteQuestionRepositoryStub, 'delete')
+    await sut.delete(makeFakeDeleteQuestion())
+    expect(deleteSpy).toHaveBeenCalledWith(makeFakeDeleteQuestion())
+  })
+
+  test('Should throw if DeleteQuestionRepository throws', async () => {
+    const { sut, deleteQuestionRepositoryStub } = makeSut()
+    jest
+      .spyOn(deleteQuestionRepositoryStub, 'delete')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+    const promise = sut.delete(makeFakeDeleteQuestion())
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return an Response on success', async () => {
+    const { sut } = makeSut()
+    const deleteResponse = await sut.delete(makeFakeDeleteQuestion())
+    expect(deleteResponse).toBeTruthy()
+  })
+})
