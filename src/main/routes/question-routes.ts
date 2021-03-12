b7ad77c2197fca16837ee54a52df4c4632465415
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-12 19:20:42 -0300

    feat: ensure QuestionRoutes should return question on success when add question

---
+++ src/main/routes/question-routes.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Router } from 'express'
+import { AdaptRoute } from '../adapters/express.route-adapter'
+import { makeQuestionController } from '../factories/question-factory'
+
+export default (router: Router): void => {
+  router.post('/question/new', AdaptRoute.add(makeQuestionController()))
+}
