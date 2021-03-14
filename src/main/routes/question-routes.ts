860d555b955fb53a1a00bc0baa7fdfd973cfd132
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2021-03-14 10:31:45 -0300

    feat: ensure Question Routes return question on get success

--- src/main/routes/question-routes.ts
+++ src/main/routes/question-routes.ts
@@ -5,5 +5,6 @@ import { makeQuestionController } from '../factories/question-factory'
 
 export default (router: Router): void => {
   router.post('/question/new', AdaptRoute.add(makeQuestionController()))
+  router.get('/question', AdaptRoute.add(makeQuestionController()))
   router.get('/questions', AdaptRoute.add(makeQuestionController()))
 }
